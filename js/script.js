9ab2fa7a425f48e0addf77baf1aae0c3d6d91d8e
DebarunMitra <debarun96mitra@gmail.com>
2019-08-31 00:14:51 +0530

    Version 1.3
    third upload
    js,css,html updated

--- js/script.js
+++ js/script.js
@@ -3,58 +3,86 @@ author:Debarun Mitra
 application name:Weather Report application
 objective: Update the user with current weather according to there location
 */
-let date=new Date();
-let h=date.getHours();
-let min=date.getMinutes();
-let day=date.getDay();
-let ampm,h1;
-const days = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
-if(h>=12)
+class Weather
 {
-  ampm='PM';
+  getDayTime(date)
+  {
+    let h=date.getHours();
+    let min=date.getMinutes();
+    let day=date.getDay();
+    let ampm,h1,time,p;
+    const days = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+    (h>=12)?ampm='PM':ampm='AM';
+    (min<10)?min="0"+min:min=min;
+    h=h%12;h1=h;
+    if(h==0){h=12;p=0;}
+    else if(h<10){h=h;p=h;}
+    (h1<10)?h1="0"+h1:h1=h1;
+    time=days[day]+", "+h1+":"+min+" "+ampm;
+    return time;
+  }
 }
-else {
-  ampm='AM';
+Weather.prototype.getTempCel=function(){
+let valueC=document.getElementById('cel').innerHTML;
+ let ipChecker=document.getElementById('inputChecker').value;
+  let intValCh=parseInt(ipChecker);
+  let temp=document.getElementById('temp').innerHTML;
+  if(valueC==='℃' && intValCh===2)
+  {
+    let num=(5*(temp-32))/9;
+    document.getElementById('temp').innerHTML=num.toFixed(0);
+    document.getElementById('cel').style.color='#1E90FF';
+    document.getElementById('fahr').style.color='#000000';
+    document.getElementById('inputChecker').value=1;
+  }
 }
-h=h%12;
-if(h==0)
-{
-    h=12;
-    p=0;
+Weather.prototype.getTempFahr=function(){
+let valueF=document.getElementById('fahr').innerHTML;
+ let ipChecker=document.getElementById('inputChecker').value;
+  let intValCh=parseInt(ipChecker);
+  let temp=document.getElementById('temp').innerHTML;
+  if(valueF==='℉' && intValCh===1)
+    {
+    let num=((temp*9)/5)+32;
+      document.getElementById('temp').innerHTML=num.toFixed(0);
+      document.getElementById('cel').style.color='#000000';
+      document.getElementById('fahr').style.color='#1E90FF';
+      document.getElementById('inputChecker').value=2;
+    }
 }
-else if(h<10)
-{
-  h=h;
-  p=h;
+Weather.prototype.getCityState=function(){
+  const searchInput=document.getElementById('inputSearch');
+  const suggVal=document.getElementById('suggestedVal');
+  const cityAndState=document.getElementById('cityNname');
+  const value=searchInput.value;
+  suggVal.innerHTML='';
+  const suggestValue=city.filter(function(cities){
+    return cities.name.toLowerCase().startsWith(value);
+ });
+ suggestValue.forEach(function(suggested){
+   const div=document.createElement('div');
+   div.innerHTML=suggested.name+", "+suggested.state;
+   div.addEventListener('click',function(){
+     searchInput.value=suggested.name+", "+suggested.state;
+     document.getElementById('cityName').innerHTML=suggested.name+", "+suggested.state;
+     document.getElementById('temp').innerHTML=suggested.temp;
+     document.getElementById('weatherImg').src=suggested.weather;
+     document.getElementById('cel').style.color='#1E90FF';
+     document.getElementById('fahr').style.color='#000000';
+     document.getElementById('inputChecker').value=1;
+     document.getElementById('report').innerHTML=suggested.report;
+     suggVal.innerHTML='';
+   });
+   suggVal.appendChild(div);
+});
+(value=='')?suggVal.innerHTML='':false;
 }
-h1=h;
-if(min<10)
-{
-    min="0"+min;
-}
-else
-{
-    min=min;
-}
-if(h1<10)
-{
-    h1="0"+h1;
-}
-else
-{
-    h1=h1;
-}
-//alert(h);
-//alert(ampm);
-time=h1+":"+min+" "+ampm;
-//alert(time);
-//alert(days[day]);
 const city=[
   {name:'Kolkata',state:'West Bengal',temp:30,weather:'./images/thunder_storm.png',report:'Thunder Storm'},
-  {name:'Bengaluru',state:'Karnataka',temp:22,weather:'./images/clouds.png',report:'Clouds'},
-  {name:'Hyderabad',state:'Andhra Pradesh',temp:25,weather:'./images/clouds.png',report:'Clouds'},
-  {name:'Itanagar',state:'Arunachal Pradesh',temp:20,weather:'./images/storm.png',report:'Storm'},
-  {name:'Dispur',state:'Assam',temp:20,weather:'./images/clouds.png',report:'Clouds'},
+  {name:'Bengaluru',state:'Karnataka',temp:22,weather:'./images/clouds.png',report:'Partly Cloudy'},
+  {name:'Hyderabad',state:'Andhra Pradesh',temp:25,weather:'./images/clouds.png',report:'Partly Cloudy'},
+  {name:'Itanagar',state:'Arunachal Pradesh',temp:20,weather:'./images/storm.png',report:'Heavy Storm'},
+  {name:'Dispur',state:'Assam',temp:20,weather:'./images/clouds.png',report:'Partly Clouds'},
   {name:'Patna',state:'Bihar',temp:35,weather:'./images/sunny.png',report:'Sunny'},
   {name:'Panaji',state:'Goa',temp:28,weather:'./images/rain.png',report:'Rain'},
   {name:'Gandhinagar',state:'Gujarat',temp:35,weather:'./images/sunny-cloudy.png',report:'Sunny Cloudy'},
@@ -62,9 +90,9 @@ const city=[
   {name:'Shimla',state:'Himachal Pradesh',temp:15,weather:'./images/cold.jpg',report:'Cold'},
   {name:'Ranchi',state:'Jharkhand',temp:32,weather:'./images/thunder_storm.png',report:'Thunder Storm'},
   {name:'Thiruvananthapuram',state:'Kerala',temp:25,weather:'./images/rain.png',report:'Rain'},
-  {name:'	Bhopal',state:'Madhya Pradesh',temp:30,weather:'./images/thunder_storm.png',report:'Thunder Storm'},
+  {name:'Bhopal',state:'Madhya Pradesh',temp:30,weather:'./images/thunder_storm.png',report:'Thunder Storm'},
   {name:'Mumbai',state:'Maharashtra',temp:36,weather:'./images/sunny.png',report:'Sunny'},
-  {name:'Bhubaneswar',state:'Odisha',temp:28,weather:'./images/clouds.png',report:'Clouds'},
+  {name:'Bhubaneswar',state:'Odisha',temp:28,weather:'./images/clouds.png',report:'Partly Cloudy'},
   {name:'Jaipur',state:'Rajasthan',temp:38,weather:'./images/sunny.png',report:'Sunny'},
   {name:'Chennai',state:'Tamil Nadu',temp:32,weather:'./images/sunny-cloudy.png',report:'Sunny Cloudy'},
   {name:'Aizawl',state:'Mizoram',temp:25,weather:'./images/thunder_storm.png',report:'Thunder Storm'},
@@ -72,73 +100,21 @@ const city=[
   {name:'Chandigarh',state:'Chandigarh',temp:25,weather:'./images/thunder_storm.png',report:'Thunder Storm'},
   {name:'Coimbatore',state:'Tamil Nadu',temp:25,weather:'./images/sunny-cloudy.png',report:'Sunny Cloudy'}
 ];
-
+let dayTime=new Date();
+const weatherObj=new Weather();
+/*initial data start*/
 document.getElementById('cityName').innerHTML=city[0].name+", "+city[0].state;
-document.getElementById('dayTime').innerHTML=days[day]+", "+time;
+document.getElementById('dayTime').innerHTML="Wednesday, 1:00PM";
 document.getElementById('temp').innerHTML=city[0].temp;
 document.getElementById('inputChecker').value=1;
 document.getElementById('report').innerHTML=city[0].report;
-/*Calculate Temparature start*/
-function temparatureConverter(value)
-{
-  let ipChecker=document.getElementById('inputChecker').value;
-  let intValCh=parseInt(ipChecker);
-  let c=document.getElementById('cel');
-  let f=document.getElementById('fahr');
-  let temp=document.getElementById('temp').innerHTML;
-//  alert(typeof(intValCh));
-//  alert(value);
-  if(value===1 && intValCh===2)
-  {
-    document.getElementById('inputChecker').value=2;
-    let num=(5*(temp-32))/9;
-    document.getElementById('temp').innerHTML=num.toFixed(0);
-    c.style.color='blue';
-    f.style.color='black';
-        document.getElementById('inputChecker').value=1;
-  }
-  else if(value===2 && intValCh===1)
-    {
-    document.getElementById('inputChecker').value=2;
-    let num=((temp*9)/5)+32;
-      document.getElementById('temp').innerHTML=num.toFixed(0);
-      c.style.color='black';
-      f.style.color='blue';
-    }
-}
-/*Calculate Temparature end*/
-
+document.getElementById('dayTime').innerHTML=weatherObj.getDayTime(dayTime);
+/*initial data stop*/
+/*temperature convert start*/
+document.getElementById('cel').addEventListener('click',weatherObj.getTempCel, false);
+document.getElementById('fahr').addEventListener('click',weatherObj.getTempFahr, false);
+/*temperature convert stop*/
 /*autocomplete start*/
 const searchInput=document.getElementById('inputSearch');
-const suggVal=document.getElementById('suggestedVal');
-const cityAndState=document.getElementById('cityNname');
-searchInput.addEventListener('keyup',function(){
-  const value=searchInput.value;
-  suggVal.innerHTML='';
-  const suggestValue=city.filter(function(cities){
-    return cities.name.toLowerCase().startsWith(value);
- });
-  suggestValue.forEach(function(suggested){
-  //  console.log(suggested.name);
-    const div=document.createElement('div');
-    div.innerHTML=suggested.name+", "+suggested.state;
-    div.addEventListener('click',function(){
-      //alert(suggested.name+", "+suggested.state);
-      searchInput.value=suggested.name+", "+suggested.state;
-      document.getElementById('cityName').innerHTML=suggested.name+", "+suggested.state;
-      document.getElementById('temp').innerHTML=suggested.temp;
-      document.getElementById('weatherImg').src=suggested.weather;
-      document.getElementById('cel').style.color='blue';
-      document.getElementById('fahr').style.color='black';
-      document.getElementById('report').innerHTML=suggested.report;
-      suggVal.innerHTML='';
-    });
-    suggVal.appendChild(div);
- });
- if(value=='')
- {
-   suggVal.innerHTML='';
- }
-});
-
+searchInput.addEventListener('keyup',weatherObj.getCityState,false);
 /*autocomplete end*/
