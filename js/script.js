0847ded6b832d4cb4358e29171538a517fce5777
DebarunMitra <50083511+DebarunMitra@users.noreply.github.com>
2019-10-04 11:27:54 +0530

    Version 1.2

--- js/script.js
+++ js/script.js
@@ -30,6 +30,7 @@ class Weather
     const suggVal=document.getElementById('suggestedVal');
     const cityAndState=document.getElementById('cityNname');
     const valCity=searchInput.value;
+    console.log(valCity);
     suggVal.innerHTML=' ';
     if(valCity)
     {
@@ -38,6 +39,7 @@ class Weather
         let nameCity=value.filter((item)=>item.name.toLowerCase().startsWith(valCity)).slice(0, 5).map((item)=>item);
           nameCity.map((item) => {
             const div=document.createElement('div');
+            console.log(item.name+', '+item.country);
              div.innerHTML=item.name+', '+item.country;
              div.addEventListener('click',function(){
              searchInput.value=item.name;
